8690a7e7deb38a1d4f8f6f8b133b2eeefd28b054
Manoj <manojlds@gmail.com>
2014-06-26 23:23:29 +0530

    Adding more parameters

--- src/main/java/com/stacktoheap/go/plugin/task/powershell/PowershellTask.java
+++ src/main/java/com/stacktoheap/go/plugin/task/powershell/PowershellTask.java
@@ -15,6 +15,7 @@ public class PowershellTask implements Task {
     public static final String BITNESS = "Bitness";
     public static final String FILE = "File";
     public static final String NOPROFILE = "NoProfile";
+    public static final String BYPASS = "Bypass";
     public static final String PARAMETERS = "Parameters";
 
     @Override
@@ -23,6 +24,7 @@ public class PowershellTask implements Task {
         config.addProperty(BITNESS);
         config.addProperty(FILE);
         config.addProperty(NOPROFILE);
+        config.addProperty(BYPASS);
         config.addProperty(PARAMETERS);
         return config;
     }
--- src/main/java/com/stacktoheap/go/plugin/task/powershell/PowershellTaskExecutor.java
+++ src/main/java/com/stacktoheap/go/plugin/task/powershell/PowershellTaskExecutor.java
@@ -3,10 +3,17 @@ package com.stacktoheap.go.plugin.task.powershell;
 import com.thoughtworks.go.plugin.api.response.execution.ExecutionResult;
 import com.thoughtworks.go.plugin.api.task.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.util.*;
 
 public class PowershellTaskExecutor implements TaskExecutor {
+
+    private static final Map<String, String> PowershellPath = new HashMap<String, String>();
+
+    static {
+        PowershellPath.put("x86", "C:\\Windows\\SysWOW64\\WindowsPowerShell\\v1.0\\powershell.exe");
+        PowershellPath.put("x64", "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe");
+    }
 
     @Override
     public ExecutionResult execute(TaskConfig taskConfig, TaskExecutionContext taskExecutionContext) {
@@ -35,17 +42,26 @@ public class PowershellTaskExecutor implements TaskExecutor {
     }
 
     ProcessBuilder createPowershellCommand(TaskExecutionContext taskContext, TaskConfig taskConfig) {
+        String bitness = taskConfig.getValue(PowershellTask.BITNESS);
         String scriptFile = taskConfig.getValue(PowershellTask.FILE);
         String noProfile = taskConfig.getValue(PowershellTask.NOPROFILE);
+        String bypassExecutionPolicy = taskConfig.getValue(PowershellTask.BYPASS);
 
         List<String> command = new ArrayList<String>();
-        command.add("powershell");
+        command.add(PowershellPath.get(bitness));
+        command.add("-NonInteractive");
         command.add("-File");
         command.add(scriptFile);
-        if (noProfile.equals("true")) {
+        if (noProfile != null && noProfile.equals("true")) {
             command.add("-NoProfile");
         }
+        if (bypassExecutionPolicy != null && bypassExecutionPolicy.equals("true")) {
+            command.add("-ExecutionPolicy ByPass");
+            command.add("ByPass");
+        }
 
-        return new ProcessBuilder(command);
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+        processBuilder.directory(new File(taskContext.workingDir()));
+        return processBuilder;
     }
 }
